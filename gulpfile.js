ba577acad0fcdf5b0e477ad46a9181492da1daaa
bbouski <bart.bouwhuis@sharepointgurus.net>
2016-08-05 17:50:55 +0200

    new attempt merging changes gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@
 
 let gulp = require("gulp"),
 	gutil = require("gulp-util"),
+	watch = require("gulp-watch"),
     eslint = require("gulp-eslint"),
     webpack = require('webpack'),
-    server = require("webpack-dev-server"),
     config = require("./webpack.config.js"),
 	o365 = require("./o365-user.js"),
 	spsave = require('gulp-spsave');
+
+var spSaveConfig = {
+			"username": o365.username,
+			"password": o365.password,
+			"siteUrl": o365.site,
+			"folder": 'SiteAssets'
+};
 
 gulp.task("lint", () => {
 	return gulp.src("./src/**/*.js")
@@ -59,12 +66,9 @@ var devConfig = Object.create(config);
 devConfig.devtool = "sourcemap";
 devConfig.debug = true;
 
-// create a single instance of the compiler to allow caching
-var devCompiler = webpack(devConfig);
-
 gulp.task("webpack:build-dev", function(callback) {
 	// run webpack
-	devCompiler.run(function(err, stats) {
+	webpack(devConfig, function(err, stats) {
 		if(err) throw new gutil.PluginError("webpack:build-dev", err);
 		gutil.log("[webpack:build-dev]", stats.toString({
 			colors: true
@@ -73,3 +77,23 @@ gulp.task("webpack:build-dev", function(callback) {
 	});
 });
 
+gulp.task("webpack:build-dev-watch", function () {
+	//set webpack watch
+	var devWatchConfig = Object.create(devConfig);
+	devWatchConfig.watch = true;
+	// run webpack
+	webpack(devWatchConfig, function (err, stats) {
+		if (err) throw new gutil.PluginError("webpack:build-dev-watch", err);
+		gutil.log("[webpack:build-dev-watch]", stats.toString({
+			colors: true
+		}));
+	});
+});
+
+//Watcher task. 
+//This taks will upload a new dev build and continue watching for any changes in the dist/SiteAssets/* files and then upload changes to SharePoint.
+gulp.task("deploy-watch", ["webpack:build-dev-watch"], function () {
+	//need awaitWritefinish to prevent uploading twice https://github.com/paulmillr/chokidar#api
+	watch('./dist/SiteAssets/**/*', { awaitWriteFinish: true })
+		.pipe(spsave(spSaveConfig));
+});
