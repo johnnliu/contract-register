28488c4ed2f3fa26727ec9012c4701dc40e4d9ef
bbouski <bart.bouwhuis@sharepointgurus.net>
2016-08-05 17:43:19 +0200

    Revert "added watch tasks"
    This reverts commit da24279a2570a7c67f2a6b825e4d1da06cbcac0d.

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,12 @@
 
 let gulp = require("gulp"),
 	gutil = require("gulp-util"),
-	watch = require("gulp-watch"),
     eslint = require("gulp-eslint"),
     webpack = require('webpack'),
     server = require("webpack-dev-server"),
     config = require("./webpack.config.js"),
 	o365 = require("./o365-user.js"),
 	spsave = require('gulp-spsave');
-
-var spSaveConfig = {
-			"username": o365.username,
-			"password": o365.password,
-			"siteUrl": o365.site,
-			"folder": 'SiteAssets'
-		};
 
 gulp.task("lint", () => {
 	return gulp.src("./src/**/*.js")
@@ -25,7 +17,7 @@ gulp.task("lint", () => {
 
 gulp.task("build", ["lint", "webpack:build"]);
 
-gulp.task("webpack:build", function (callback) {
+gulp.task("webpack:build", function(callback) {
 	// modify some webpack config options
 	var buildConfig = Object.create(config);
 	buildConfig.plugins = buildConfig.plugins.concat(
@@ -40,8 +32,8 @@ gulp.task("webpack:build", function (callback) {
 	);
 
 	// run webpack
-	webpack(buildConfig, function (err, stats) {
-		if (err) throw new gutil.PluginError("webpack:build", err);
+	webpack(buildConfig, function(err, stats) {
+		if(err) throw new gutil.PluginError("webpack:build", err);
 		gutil.log("[webpack:build]", stats.toString({
 			colors: true
 		}));
@@ -49,10 +41,17 @@ gulp.task("webpack:build", function (callback) {
 	});
 });
 
-gulp.task("deploy", ["webpack:build"], function () {
+gulp.task("deploy", ["webpack:build"], function(){
+
 	gulp.src('./dist/SiteAssets/**/*')
-		.pipe(
-		spsave(spSaveConfig));
+	.pipe(
+		spsave({
+			"username": o365.username,
+    		"password": o365.password,
+    		"siteUrl": o365.site,
+			"folder": 'SiteAssets' 
+		}));
+
 });
 
 // modify some webpack config options
@@ -60,10 +59,13 @@ var devConfig = Object.create(config);
 devConfig.devtool = "sourcemap";
 devConfig.debug = true;
 
-gulp.task("webpack:build-dev", function (callback) {
+// create a single instance of the compiler to allow caching
+var devCompiler = webpack(devConfig);
+
+gulp.task("webpack:build-dev", function(callback) {
 	// run webpack
-	webpack(devConfig, function (err, stats) {
-		if (err) throw new gutil.PluginError("webpack:build-dev", err);
+	devCompiler.run(function(err, stats) {
+		if(err) throw new gutil.PluginError("webpack:build-dev", err);
 		gutil.log("[webpack:build-dev]", stats.toString({
 			colors: true
 		}));
@@ -71,24 +73,3 @@ gulp.task("webpack:build-dev", function (callback) {
 	});
 });
 
-
-gulp.task("webpack:build-dev-watch", function () {
-	//set webpack watch
-	var devWatchConfig = Object.create(devConfig);
-	devWatchConfig.watch = true;
-	// run webpack
-	webpack(devWatchConfig, function (err, stats) {
-		if (err) throw new gutil.PluginError("webpack:build-dev-watch", err);
-		gutil.log("[webpack:build-dev-watch]", stats.toString({
-			colors: true
-		}));
-	});
-});
-
-//Watcher task. 
-//This taks will upload a new dev build and continue watching for any changes in the dist/SiteAssets/* files and then upload changes to SharePoint.
-gulp.task("deploy-watch", ["webpack:build-dev-watch"], function () {
-	//need awaitWritefinish to prevent uploading twice https://github.com/paulmillr/chokidar#api
-	watch('./dist/SiteAssets/**/*', { awaitWriteFinish: true })
-		.pipe(spsave(spSaveConfig));
-});
